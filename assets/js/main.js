5a378d35c4aa900569e3ee8ea7b4ab9be5ee8587
Siniavskii Alexandr <siniavskiialexandr@gmail.com>
2021-03-21 16:03:54 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+	$('.header-navbar__btn').on('click', function(){
+		$(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').stop(true,true).fadeToggle(500)
+	});
+});
